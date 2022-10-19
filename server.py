bf181032714b300c8a7d323382026b2ace304093
abhinav <abinavmk00@gmail.com>
2022-10-19 09:01:09 +0530

    server and client scripts added

--- server.py
+++ server.py
@@ -1,2 +1,41 @@
-import socket
+#server
 
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!disconnect"
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[New connection : {addr} ]")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MSG :
+                connected = False
+            print(f"[{addr}] {msg}")
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[Listening : {SERVER}]")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f"[Active connection : {threading.active_count()-1} ]")
+
+print("[Starting]")
+start()
